2fc76551291ea7e56bc069e2d0682a6f224b44d4
Arthur Ishiguro <arthuri@google.com>
2021-11-13 00:16:49 +0000

    Reconnect to all hidl sensors HALs
    Fixes: 206162862
    Test: Restart sensors HAL and verify resume
    Change-Id: Iba93246a30565783a0634a9a0cd966a7ccf841d3
    (cherry picked from commit 4b26b7fb4604ded2ec4e2bb3e4f40a76c48c6fcf)

--- services/sensorservice/SensorDevice.cpp
+++ services/sensorservice/SensorDevice.cpp
@@ -325,7 +325,7 @@ void SensorDevice::reconnect() {
     mActivationCount.clear();
     mSensorList.clear();
 
-    if (connectHidlServiceV2_0() == HalConnectionStatus::CONNECTED) {
+    if (connectHidlService()) {
         initializeSensorList();
 
         if (sensorHandlesChanged(previousSensorList, mSensorList)) {

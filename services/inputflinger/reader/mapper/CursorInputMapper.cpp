8d4a700c6ff455398fb555f5ec981d88476f35f9
hmtheboy154 <buingoc67@gmail.com>
2023-05-17 03:45:32 -0400

    Revert "inputflinger: fix relative mouse movement"
    This reverts commit 8afb2d61e174319d8de1774d80816ee1245aa380.

--- services/inputflinger/reader/mapper/CursorInputMapper.cpp
+++ services/inputflinger/reader/mapper/CursorInputMapper.cpp
@@ -393,7 +393,7 @@ void CursorInputMapper::sync(nsecs_t when, nsecs_t readTime) {
         // Rotate delta according to orientation if needed.
         rotateDelta(mOrientation, &deltaX, &deltaY);
         mPointerVelocityControl.move(when, &deltaX, &deltaY);
-        if (mSource == AINPUT_SOURCE_MOUSE) {
+        if (mPointerController != NULL) {
             if (moved) {
                 mPointerController->move(deltaX, deltaY);
             }
